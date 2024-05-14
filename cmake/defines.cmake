ecfc9d72c73b881a9e9e03da14b3ef068d50a75b
nelbok <nelbok@users.noreply.github.com>
2024-05-14 13:14:30 +0200

    Version 0.1.1

--- cmake/defines.cmake
+++ cmake/defines.cmake
@@ -3,7 +3,7 @@ set(CFS_ORGANIZATION "FoxxySoft")
 string(TIMESTAMP FS_YEAR %Y)
 set(FS_COPYRIGHT "Copyright (C) 2011-${FS_YEAR} ${FS_ORGANIZATION}")
 
-set(LH_VERSION "0.1.0")
+set(LH_VERSION "0.1.1")
 string(REPLACE "." "," LH_VERSION_RC ${LH_VERSION})
 set(CLH_DATA "LHData")
 set(CLH_EDITOR "LHEditor")
